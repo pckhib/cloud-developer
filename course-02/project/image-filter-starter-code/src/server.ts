a4672cd2618b71f7ea9705c3bb2d002aff42d863
Patrick Hieber <patrickhieber92@gmail.com>
2020-02-10 20:28:15 +0100

    Add filteredimage endpoint
    Add a new endpoint "filteredimage" wich requests an image based on a
    URL, perform various filter and return the image.

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -28,6 +29,22 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+
+  app.get("/filteredimage", async(req: Request, res: Response) => {
+    let { image_url } = req.query;
+
+    if (!image_url) {
+      return res.status(400).send(`image_url is required.`);
+    }
+
+    const path = await filterImageFromURL(image_url);
+
+    res.sendFile(path, err => {
+      if (!err) {
+        deleteLocalFiles(Array(path));
+      }
+    });
+  });
 
   //! END @TODO1
   
